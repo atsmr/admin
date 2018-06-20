41f8816dd9ac42f29e21f4c1dbd17170c0b88122
Ryohei <ano.lightup@gmail.com>
2018-06-20 20:54:55 +0900

    modified

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -2,6 +2,7 @@ import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import { connect } from 'preact-redux'
 import style from './style';
+import ADD from '../../../assets/add.svg';
 import * as actions from '../../../actions'
 import reducer from '../../../reducer'
 //import * as firebase from 'firebase'
@@ -15,9 +16,9 @@ class Button extends Component {
 	}
 	render() {
 		return (
-			<button class={ this.props.s.test ? style.dark: style.light }>{this.props.title}</button>
+		<button class={ this.props.s.test ? style.dark: style.light }><img class={style.add} src={ADD} width="2" height="2" alt="+" />{this.props.title}</button>
 		)
 	}
-}
+}	
 
 export default Button;
